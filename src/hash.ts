5b1f2a51f26644a2223530c84bd7ac25df8905a0
Jamie C <jcoup@me.com>
2015-07-18 01:41:27 +0100

    added sha256 and  hmac-sha256

--- src/hash.ts
+++ src/hash.ts
@@ -1,17 +1,27 @@
+
 import {HashType} from './enums/hash_type';
 import {Rusha} from './../node_modules/rusha/rusha';
+import {SHA256} from './../node_modules/fast-sha256/sha256';
 import {Hexadecimal} from './../node_modules/encode.ts/encode';
+
+
 
 export class Hash {
 
   public static createHash(data: Uint8Array, hash: HashType): Uint8Array {
 
-    if(hash === HashType.SHA1) {
-      return Hexadecimal.getBytes(new Rusha().digest(data));
-    } else {
-      throw 'Unimplemented';
+    switch(hash) {
+      case HashType.SHA1 :
+        return Hexadecimal.getBytes(new Rusha().digest(data));
+        break;
+
+      case HashType.SHA256 :
+        return SHA256.HASH(data);
+        break;
+
+      default :
+        throw 'Unimplemented';
+        break;
     }
-
   }
-
 }
